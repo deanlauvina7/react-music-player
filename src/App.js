431eddf7dbcb049e9498b5f5cc48a1b0b872a7df
deanlauvina7 <deanlauvina7@gmail.com>
2020-11-13 11:41:20 -0600

    Set state for isplaying

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './styles/app.scss';
+import data from './util';
+// @import url('https://fonts.googleapis.com/css2?family=Poppins&display=swap');
 // Adding Components
 import Player from './components/Player';
 import Song from './components/Song';
 
 function App() {
+  // State
+  const [songs, useSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
   return (
     <div className="App">
-      <Song />
-      <Player />
+      <Song currentSong={currentSong} />
+      <Player
+        setIsPlaying={setIsPlaying}
+        isPlaying={isPlaying}
+        currentSong={currentSong}
+      />
     </div>
   );
 }
