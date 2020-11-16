93ea4acf06001ea5dc03e7ac7485e7426020b50a
deanlauvina7 <deanlauvina7@gmail.com>
2020-11-16 10:12:39 -0600

    Customized the slider

--- src/App.js
+++ src/App.js
@@ -6,8 +6,6 @@ import Player from './components/Player';
 import Song from './components/Song';
 import Library from './components/Library';
 import Nav from './components/Nav';
-import { library } from '@fortawesome/fontawesome-svg-core';
-
 function App() {
   // Ref
   const audioRef = useRef(null);
@@ -18,12 +16,22 @@ function App() {
   const [songInfo, setSongInfo] = useState({
     currentTime: 0,
     duration: 0,
+    animationPercentage: 0,
   });
   const [libraryStatus, setLibraryStatus] = useState(false);
   const timeUpdateHandler = (e) => {
     const current = e.target.currentTime;
     const duration = e.target.duration;
-    setSongInfo({ ...songInfo, currentTime: current, duration });
+    // Calculate percentage
+    const roundedCurrent = Math.round(current);
+    const roundedDuration = Math.round(duration);
+    const animation = Math.round((roundedCurrent / roundedDuration) * 100);
+    setSongInfo({
+      ...songInfo,
+      currentTime: current,
+      duration,
+      animationPercentage: animation,
+    });
   };
   return (
     <div className="App">
