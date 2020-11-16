92d390c84a686afeb1ccd5d3cde4563f7750ddde
deanlauvina7 <deanlauvina7@gmail.com>
2020-11-15 21:03:43 -0600

    Created the selected songs

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
         isPlaying={isPlaying}
         currentSong={currentSong}
       />
-      <Library songs={songs} />
+      <Library songs={songs} setCurrentSong={setCurrentSong} />
     </div>
   );
 }
